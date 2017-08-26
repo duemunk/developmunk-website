4bb7e0be0c3770a5d1d69cf10b9855587c204400
Andreas Larsen <andreas@larsenwork.com>
2017-08-26 22:06:00 +0200

    Form as snippet

---
+++ site/snippets/slor-form.php
@@ -0,0 +1,12 @@
+<form 
+    action="//developmunk.us16.list-manage.com/subscribe/post?u=6355a7613325da59aa82b5576&amp;id=7d906bb182"
+    method="post"
+    name="mc-embedded-subscribe-form"
+    target="_blank" novalidate>
+    <input type="email" value="" name="EMAIL" placeholder="E-mail address">
+    <input type="submit" value="Notify me on launch" name="subscribe">
+    <div id="mce-responses">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>
+</form>
--- site/templates/slor.php
+++ site/templates/slor.php
@@ -42,19 +42,8 @@
     </header>
 
     <main>
+      <?php snippet('slor-form') ?>
 
-      <form 
-        action="//developmunk.us16.list-manage.com/subscribe/post?u=6355a7613325da59aa82b5576&amp;id=7d906bb182"
-        method="post"
-        name="mc-embedded-subscribe-form"
-        target="_blank" novalidate>
-        <input type="email" value="" name="EMAIL" placeholder="E-mail address">
-        <input type="submit" value="Notify me on launch" name="subscribe">
-        <div id="mce-responses">
-          <div class="response" id="mce-error-response" style="display:none"></div>
-          <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>
-      </form>
       <p>Slør launches along with iOS 11 mid-September 2017.</p>
 
       <div id="quotes" class="margin equal-columns">
