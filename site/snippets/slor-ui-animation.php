0ff32a4e7c68a3ee9b6edd4b8aa0424b90df9716
Andreas Larsen <andreas@larsenwork.com>
2017-08-27 15:06:53 +0200

    More explicit style logic

--- site/snippets/slor-ui-animation.php
+++ site/snippets/slor-ui-animation.php
@@ -4,36 +4,38 @@
 
   <div class="demo-phone">
     <div 
-      class="demo-phone-bezel"
+      class="demo-phone-device"
       v-on:mouseover="stopRepeat"
       v-on:mouseleave="startRepeat"
       v-on:touchstart="stopRepeat"
       v-on:touchend="startRepeat">
-      <div class="demo-img-wrap">
-        <div 
-          class="demo-focus demo-focus--top"
-          v-on:click="setFocus(1)">
+      <div class="demo-phone-device-content">
+        <div class="demo-img-wrap">
           <div 
-            v-if="focus.position == 1"
-            class="demo-focus-square"></div>
+            class="demo-focus demo-focus--top"
+            v-on:click="setFocus(1)">
+            <div 
+              v-if="focus.position == 1"
+              class="demo-focus-square"></div>
+          </div>
+          <div 
+            class="demo-focus demo-focus--middle"
+            v-on:click="setFocus(2)">
+            <div class="demo-focus-square" v-if="focus.position == 2"></div>
+          </div>
+          <div
+            class="demo-focus demo-focus--bottom"
+            v-on:click="setFocus(3)">
+            <div class="demo-focus-square" v-if="focus.position == 3"></div>
+          </div>
+          <img v-bind:src="'assets/images/slor/' + focus.position + '.' + blur.amount + '.jpg'" />
         </div>
-        <div 
-          class="demo-focus demo-focus--middle"
-          v-on:click="setFocus(2)">
-          <div class="demo-focus-square" v-if="focus.position == 2"></div>
-        </div>
-        <div
-          class="demo-focus demo-focus--bottom"
-          v-on:click="setFocus(3)">
-          <div class="demo-focus-square" v-if="focus.position == 3"></div>
-        </div>
-        <img v-bind:src="'assets/images/slor/' + focus.position + '.' + blur.amount + '.jpg'" />
+        <input
+          type="range"
+          v-model.number="blur.amount"
+          v-bind:min="blur.min"
+          v-bind:max="blur.max">
       </div>
-      <input
-        type="range"
-        v-model.number="blur.amount"
-        v-bind:min="blur.min"
-        v-bind:max="blur.max">
     </div>
   </div>
 
