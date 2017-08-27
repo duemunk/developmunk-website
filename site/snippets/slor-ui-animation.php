c584e1279269aea75cb4cddd10ede9cb800ac9e3
Andreas Larsen <andreas@larsenwork.com>
2017-08-27 11:02:19 +0200

    Fix merge errors

--- site/snippets/slor-ui-animation.php
+++ site/snippets/slor-ui-animation.php
@@ -1,4 +1,5 @@
 <div id="app">
+
   <div>
     <div id="logo">
       <a href="/slor">
@@ -10,6 +11,9 @@
 
   <div id="demo-phone" class="">
     <div class="img-wrap">
+      <div class="focus focus--top"></div>
+      <div class="focus focus--middle"></div>
+      <div class="focus focus--bottom"></div>
       <img v-bind:src="'assets/images/slor/' + focus.amount + '.' + blur.amount + '.jpg'" />
     </div>
     <input 
@@ -21,17 +25,18 @@
       v-on:mouseleave="startRepeat"
       v-on:touchstart="stopRepeat"
       v-on:touchend="startRepeat">
-    </div>
+  </div>
 
-    <div>
+  <div>
     <div id="aperture" class="demo-text" v-bind:class="{ dimmed: !isBlurring }">
       <h2>Change Aperture.</h2>
-      <p>Change the aperture and see the amount of background blur adapt just a the bokeh effect on a DSLR.</p>
+      <p>Change the aperture and see the amount of background blur adapt just as the bokeh effect on a DSLR.</p>
     </div>
     <div id="focus" class="demo-text" v-bind:class="{ dimmed: !isFocusing }">
       <h2>Change Focus.</h2>
-      <p>Move the focus point to any place in the photo. The selected focus point will stay sharp and anything further back the to motiv will be blurred. Any Portrait Mode photo taken in iOS 11 can be edited in Slør.</p>
+      <p>Move the focus point to any place in the photo. Quickly fix mistaken auto focus.</p>
     </div>
   </div>
+
 </div>
 <?php echo js('assets/js/slor.js') ?>
