c22983e1241317871541fa95d5dc299158ca0717
Andreas Larsen <andreas@larsenwork.com>
2017-08-27 15:09:02 +0200

    Consistet snippet use

--- site/snippets/slor-form.php
+++ site/snippets/slor-form.php
@@ -1,12 +1,15 @@
-<form 
+<div class="signup">
+    <form 
     action="//developmunk.us16.list-manage.com/subscribe/post?u=6355a7613325da59aa82b5576&amp;id=7d906bb182"
     method="post"
     name="mc-embedded-subscribe-form"
     target="_blank" novalidate>
-    <input type="email" value="" name="EMAIL" placeholder="E-mail address">
-    <input type="submit" value="Notify me on launch" name="subscribe">
-    <div id="mce-responses">
-        <div class="response" id="mce-error-response" style="display:none"></div>
-        <div class="response" id="mce-success-response" style="display:none"></div>
-    </div>
-</form>
+        <input type="email" value="" name="EMAIL" placeholder="E-mail address">
+        <input type="submit" value="Notify me on launch" name="subscribe">
+        <div id="mce-responses">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>
+    </form>
+    <p>Slør launches along with iOS 11 mid-September 2017.</p>
+</div>
