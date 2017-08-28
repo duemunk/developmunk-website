991f592f2d117b76b3bea2725817cd7e0fc8d171
Tobias Due Munk <tobias@developmunk.dk>
2017-08-28 23:01:46 +0200

    Adjust site title

--- site/templates/slor.php
+++ site/templates/slor.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <title><?php echo $page->title()->html() ?> | <?php echo $site->title()->html() ?></title>
     <meta name="description" content="<?php echo $site->description()->html() ?>">
     <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
     <?php echo css('assets/css/slor/slor.css') ?>
