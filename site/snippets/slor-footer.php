35ba41bf4e9ea06bb6ab4cd6b1b2c3753f2f48e3
Tobias Due Munk <tobias@developmunk.dk>
2017-08-31 14:28:59 +0200

    Add press kit + links to it and Instagram

--- site/snippets/slor-footer.php
+++ site/snippets/slor-footer.php
@@ -1,1 +1,3 @@
-<p>Crafted bits and pixels<br/><em>in</em><br/>Copenhagen<br/><em>by</em><br/><a href="https://twitter.com/tobiasdm">Tobias Due Munk</a>, <a href="/">developmunk</a>.</p>
+<p><a href="https://instagram.com/slor_app">Instragram</a></p>
+<p><a href="http://developmunk.dk/slor/presskit.zip">Press Kit</a></p>
+<p>Crafted bits and pixels <em>by</em><br/><a href="https://twitter.com/tobiasdm">Tobias Due Munk</a>, <a href="/">developmunk</a>.</p>
