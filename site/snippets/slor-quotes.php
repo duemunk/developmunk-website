d28aeaafc2629eeab0e64acd24e9dbaa8f5290d8
Tobias Due Munk <tobias@developmunk.dk>
2017-09-21 14:21:55 +0200

    Add quote from MacStories.net

--- site/snippets/slor-quotes.php
+++ site/snippets/slor-quotes.php
@@ -1,5 +1,10 @@
 <div id="quotes" class="margin equal-columns">
   <?php snippet('slor-quote', array(
+    'author'=>'MacStories',
+    'description'=>'Slør’s simplicity makes it an intuitive, accessible way for casual photographers to play around and tweak Portrait photos to make them look best.',
+    'url'=>'https://www.macstories.net/reviews/slr-enhances-the-iphones-portrait-mode-with-depth-adjustment-tools/'
+  )) ?>
+  <?php snippet('slor-quote', array(
     'author'=>'Michael Flarup',
     'description'=>'The ability to edit focus points in portrait shots along with finetuning background blur just adds a whole new depth (pun intended) to editing.',
     'url'=>'https://www.producthunt.com/posts/slor#comment-522840'
